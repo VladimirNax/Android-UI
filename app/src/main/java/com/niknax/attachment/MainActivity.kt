520bcb9fc892609ceac7ba299bae3da3e0bcd107
vladimir <nikonnax@gmail.com>
2020-12-08 01:56:45 +0400

    Toast. метод setOnClickListener на кнопки меню

---
+++ app/src/main/java/com/niknax/attachment/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.niknax.attachment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main.setOnClickListener {
+            Toast.makeText(this, R.string.btn_main, Toast.LENGTH_SHORT).show()
+        }
+        films.setOnClickListener {
+            Toast.makeText(this, R.string.btn_films, Toast.LENGTH_SHORT).show()
+        }
+        series.setOnClickListener {
+            Toast.makeText(this, R.string.btn_series, Toast.LENGTH_SHORT).show()
+        }
+        cartoons.setOnClickListener {
+            Toast.makeText(this, R.string.btn_cartoons, Toast.LENGTH_SHORT).show()
+        }
+        news.setOnClickListener {
+            Toast.makeText(this, R.string.btn_news, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
