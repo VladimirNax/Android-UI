6058b2480b9e20b19d9142679a712d27484c5810
vladimir <nikonnax@gmail.com>
2021-01-08 01:25:40 +0400

    Меню на экране деталей фильма Collapsing Toolbar. Snackbar на нажатие кнопок В избранное и Посмотреть позже .

--- app/src/main/java/com/niknax/attachment/DetailsActivity.kt
+++ app/src/main/java/com/niknax/attachment/DetailsActivity.kt
@@ -2,7 +2,12 @@ package com.niknax.attachment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.activity_details.view.*
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +17,26 @@ class DetailsActivity : AppCompatActivity() {
         //Получаем наш фильм из переданного бандла
         val film = intent.extras?.get("film") as Film
         bind(film)
+
+//нажатие на кнопки меню. сообщение Snackbar
+        toolbar_menu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.favorites_details -> {
+                    Snackbar.make(details_layout, "Избранное", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later_details -> {
+                    Snackbar.make(details_layout, "Посмотреть позже", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+// Тост при  клике на иконку details_fab
+        details_fab.setOnClickListener {
+            Toast.makeText(this, "details_fab", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 
