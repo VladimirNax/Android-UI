2f42dd32d10334399653ec194299731c29ca546e
vladimir <nikonnax@gmail.com>
2021-02-27 00:04:25 +0400

    Custom View

---
+++ app/src/main/java/com/niknax/attachment/CustomView/RatingDonutView.kt
@@ -0,0 +1,8 @@
+package com.niknax.attachment.CustomView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class RatingDonutView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null) : View(context, attributeSet) {
+}
