b0070655948fab2e6169840fba07429f4b007c11
vladimir <nikonnax@gmail.com>
2021-03-30 00:00:57 +0400

    создание object DI

--- app/src/main/java/com/niknax/attachment/data/MainRepository.kt
+++ app/src/main/java/com/niknax/attachment/data/MainRepository.kt
@@ -1,29 +1,6 @@
 package com.niknax.attachment.data
 
-import com.niknax.attachment.R
-import com.niknax.attachment.domain.Film
 
 class MainRepository {
-    /*val filmsDataBase = listOf(
-        Film("Декстер",
-            R.drawable.p1, "By day, mild-mannered Dexter is a blood-spatter analyst for the Miami police. But at night, he is a serial killer who only targets other murderers.", 7.1f),
-        Film("Пропавшая",
-            R.drawable.p2, "A recently widowed traveler is kidnapped by a cold blooded killer, only to escape into the wilderness where she is forced to battle against the elements as her pursuer closes in on her.", 9.8f),
-        Film(" Город воров",
-            R.drawable.p3, "A longtime thief, planning his next job, tries to balance his feelings for a bank manager connected to an earlier heist, and a hell-bent F.B.I Agent looking to bring him and his crew down.", 8.6f),
-        Film("One Night in Miami",
-            R.drawable.p4, "One Night in Miami is a fictional account of one incredible night where icons Muhammad Ali, Malcolm X, Sam Cooke, and Jim Brown gathered discussing their roles in the civil rights movement and cultural upheaval of the 60s.", 2.6f),
-        Film("Последний из могикан",
-            R.drawable.p5, "Three trappers protect the daughters of a British Colonel in the midst of the French and Indian War.", 5.6f),
-        Film("Стартрек: Бесконечность",
-            R.drawable.p6, "The crew of the USS Enterprise explores the furthest reaches of uncharted space, where they encounter a new ruthless enemy, who puts them, and everything the Federation stands for, to the test.", 4.3f),
-        Film(" Шоу Трумана",
-            R.drawable.p7, "An insurance salesman discovers his whole life is actually a reality TV show.", 6.7f),
-        Film(" Любите Куперов",
-            R.drawable.p8, "The intertwined stories of four generations of Coopers unfold right before the annual family reunion on Christmas Eve. Can they survive the most beautiful time of the year?", 6.1f),
-        Film(" Гламурные боссы",
-            R.drawable.p9, "Two friends with very different ideals start a beauty company together. One is more practical while the other wants to earn her fortune and live a lavish lifestyle.", 8.1f),
-        Film(" Ритм-секция",
-            R.drawable.p10, "A woman seeks revenge against those who orchestrated a plane crash that killed her family.", 9.7f),
-    )*/
+
 }
