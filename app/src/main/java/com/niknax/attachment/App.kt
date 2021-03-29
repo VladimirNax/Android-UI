5cc7026eeb0b18f8a85e4923669c87c1f7a03689
vladimir <nikonnax@gmail.com>
2021-03-30 00:06:42 +0400

    изменение класса App - инициализация Koin

--- app/src/main/java/com/niknax/attachment/App.kt
+++ app/src/main/java/com/niknax/attachment/App.kt
@@ -1,61 +1,33 @@
 package com.niknax.attachment
 
 import android.app.Application
+import com.niknax.attachment.DI.DI
 import com.niknax.attachment.data.ApiConstants
 import com.niknax.attachment.data.MainRepository
 import com.niknax.attachment.data.TmdbApi
 import com.niknax.attachment.domain.Interactor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class App : Application() {
-    lateinit var repo: MainRepository
-    lateinit var interactor: Interactor
 
     override fun onCreate() {
         super.onCreate()
-        //Инициализируем экземпляр App, через который будем получать доступ к остальным переменным
-        instance = this
-        //Инициализируем репозиторий
-        repo = MainRepository()
-        //Инициализируем интерактор
-        //interactor = Interactor(repo)
-
-        //Создаём кастомный клиент
-        val okHttpClient = OkHttpClient.Builder()
-            //Настраиваем таймауты для медленного интернета
-            .callTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            //Добавляем логгер
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                if (BuildConfig.DEBUG) {
-                    level = HttpLoggingInterceptor.Level.BASIC
-                }
-            })
-            .build()
-        //Создаем Ретрофит
-        val retrofit = Retrofit.Builder()
-            //Указываем базовый URL из констант
-            .baseUrl(ApiConstants.BASE_URL)
-            //Добавляем конвертер
-            .addConverterFactory(GsonConverterFactory.create())
-            //Добавляем кастомный клиент
-            .client(okHttpClient)
-            .build()
-//Создаем сам сервис с методами для запросов
-        var retrofitService = retrofit.create(TmdbApi::class.java)
-//Инициализируем интерактор
-        interactor = Interactor(repo, retrofitService)
+        startKoin {
+            //Прикрепляем контекст
+            androidContext(this@App)
+            //(Опционально) подключаем зависимость
+            androidLogger()
+            //Инициализируем модули
+            modules(listOf(DI.mainModule))
+        }
 
     }
 
-    companion object {
-        //Здесь статически хранится ссылка на экземпляр App
-        lateinit var instance: App
-            //Приватный сеттер, чтобы нельзя было в эту переменную присвоить что-либо другое
-            private set
-    }
 }
