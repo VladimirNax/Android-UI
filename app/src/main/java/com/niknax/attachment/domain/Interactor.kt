d2680e5e5f9b548b823e4437d72809dd78e2bb06
vladimir <nikonnax@gmail.com>
2021-04-28 00:13:58 +0400

    Interactor - изменение метода onResponse, и еще новый getFilmsFromDB

--- app/src/main/java/com/niknax/attachment/domain/Interactor.kt
+++ app/src/main/java/com/niknax/attachment/domain/Interactor.kt
@@ -11,25 +11,40 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class Interactor(private val repo: MainRepository, private val retrofitService: TmdbApi, private val preferences: PreferenceProvider) {
+class Interactor(
+    private val repo: MainRepository,
+    private val retrofitService: TmdbApi,
+    private val preferences: PreferenceProvider
+) {
     fun getFilmsFromApi(page: Int, callback: HomeFragmentViewModel.ApiCallback) {
         //Метод getDefaultCategoryFromPreferences() будет нам получать при каждом запросе нужный нам список фильмов
-        retrofitService.getFilms(getDefaultCategoryFromPreferences(), API.KEY, "ru-RU", page).enqueue(object : Callback<TmdbResultsDto> {
-            override fun onResponse(call: Call<TmdbResultsDto>, response: Response<TmdbResultsDto>) {
-                //При успехе мы вызываем метод передаем onSuccess и в этот коллбэк список фильмов
-                callback.onSuccess(Converter.convertApiListToDtoList(response.body()?.tmdbFilms))
-            }
+        retrofitService.getFilms(getDefaultCategoryFromPreferences(), API.KEY, "ru-RU", page)
+            .enqueue(object : Callback<TmdbResultsDto> {
+                override fun onResponse(call: Call<TmdbResultsDto>, response: Response<TmdbResultsDto>) {
+                    //При успехе мы вызываем метод, передаем onSuccess и в этот коллбэк список фильмов
+                    val list = Converter.convertApiListToDtoList(response.body()?.tmdbFilms)
+                    //Кладем фильмы в бд
+                    list.forEach {
+                        repo.putToDb(film = it)
+                    }
+                    callback.onSuccess(list)
+                }
 
-            override fun onFailure(call: Call<TmdbResultsDto>, t: Throwable) {
-                //В случае провала вызываем другой метод коллбека
-                callback.onFailure()
-            }
-        })
+                override fun onFailure(call: Call<TmdbResultsDto>, t: Throwable) {
+                    //В случае провала вызываем другой метод коллбека
+                    callback.onFailure()
+                }
+            })
     }
+
     //Метод для сохранения настроек
     fun saveDefaultCategoryToPreferences(category: String) {
         preferences.saveDefaultCategory(category)
     }
+
     //Метод для получения настроек
     fun getDefaultCategoryFromPreferences() = preferences.geDefaultCategory()
+
+    //Метод дергает метод репозитория MainRepository , чтобы тот забрал фильмы из БД
+    fun getFilmsFromDB(): List<Film> = repo.getAllFromDB()
 }
