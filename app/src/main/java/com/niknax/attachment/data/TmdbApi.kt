9f2c903f60a8c4944d6d4fab11b183b428dafaf2
vladimir <nikonnax@gmail.com>
2021-07-25 22:19:37 +0400

    @Query("query") query: String

--- app/src/main/java/com/niknax/attachment/data/TmdbApi.kt
+++ app/src/main/java/com/niknax/attachment/data/TmdbApi.kt
@@ -9,8 +9,9 @@ import retrofit2.http.Query
 interface TmdbApi {
     @GET("3/movie/{category}")
     fun getFilms(
-        @Path("category") category: String,
+        //@Path("category") category: String,
         @Query("api_key") apiKey: String,
+        @Query("query") query: String,
         @Query("language") language: String,
         @Query("page") page: Int
     ): Call<TmdbResultsDto>
